0f5db2917408b35632caed48ea9add63294486d8
Kayra Czerniewicz <kayra@cuadds.com>
2020-11-06 16:00:59 -0400

    cleaned code

--- src/index.js
+++ src/index.js
@@ -20,26 +20,3 @@ const Translation = () => {
 };
 
 render(<Translation />, document.getElementById("root"));
-
-// import { es } from "make-plural/plurals";
-
-// import catalogEn from "./locales/en/messages.js";
-// import catalogEs from "./locales/es/messages.js";
-// import catalogFr from "./locales/fr/messages.js";
-
-// i18n.loadLocaleData("es", { plurals: es });
-// i18n.load({
-// 	en: catalogEn.messages,
-// 	fr: catalogFr.messages,
-// 	es: catalogEs.messages,
-// });
-// // i18n.load("es", messages);
-
-// i18n.activate("es");
-
-// const Translation = () => (
-
-// 	<I18nProvider i18n={i18n}>
-// 		<App />
-// 	</I18nProvider>
-// );

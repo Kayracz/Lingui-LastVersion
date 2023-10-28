6e6c1001d4a84fef01e3538658e95689b652c129
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-10-28 00:01:51 -0400

    Updates

---
+++ static/js/311.b77f2172.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunklastlingui=self.webpackChunklastlingui||[]).push([[311],{311:function(e){e.exports={messages:{Me:"it's me.",Salute:"HELLOO"}}}}]);
+//# sourceMappingURL=311.b77f2172.chunk.js.map
---
+++ static/js/787.5f518d96.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunklastlingui=self.webpackChunklastlingui||[]).push([[787],{787:function(e){e.exports={messages:{Me:"C'est moi.",Salute:"COUCOUUU"}}}}]);
+//# sourceMappingURL=787.5f518d96.chunk.js.map

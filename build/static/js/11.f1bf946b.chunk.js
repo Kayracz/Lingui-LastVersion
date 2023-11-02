b8d046578e6ec8d2459f301ee8176e8743e31e64
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-11-01 20:50:08 -0400

    added product build for render

---
+++ build/static/js/11.f1bf946b.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunklastlingui=self.webpackChunklastlingui||[]).push([[11],{11:function(e){e.exports={messages:{Me:"Soy yo.",Salute:"HOLAA"}}}}]);
+//# sourceMappingURL=11.f1bf946b.chunk.js.map

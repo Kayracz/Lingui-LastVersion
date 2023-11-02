6d7211fff8e479483ed918a3d20643d0e404b1ed
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-11-01 21:46:51 -0400

    created new build

---
+++ build/static/js/11.a249add5.chunk.js
@@ -0,0 +1,2 @@
+(self.webpackChunklastlingui=self.webpackChunklastlingui||[]).push([[11],{11:function(s){s.exports={messages:{Me:"Soy sho.",Salute:"HOLAA"}}}}]);
+//# sourceMappingURL=11.a249add5.chunk.js.map
--- build/static/js/11.f1bf946b.chunk.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunklastlingui=self.webpackChunklastlingui||[]).push([[11],{11:function(e){e.exports={messages:{Me:"Soy yo.",Salute:"HOLAA"}}}}]);
-//# sourceMappingURL=11.f1bf946b.chunk.js.map
